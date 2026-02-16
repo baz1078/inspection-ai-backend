2b766871d92056fa69054a50ef6533df777ce2b1
Ubuntu <ubuntu@ip-172-31-5-101.us-east-2.compute.internal>
2026-02-16 05:35:28 +0000

    Create missing database.py file

---
+++ database.py
@@ -0,0 +1,16 @@
+import os
+from sqlalchemy import create_all_engine, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# This gets the database URL from your Render environment variables
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+# If the URL starts with postgres://, we fix it for SQLAlchemy 1.4+
+if SQLALCHEMY_DATABASE_URL and SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
