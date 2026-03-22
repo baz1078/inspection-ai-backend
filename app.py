7ba0a46b8cd835b97da6f17e37948a6ef7a86595
Baz <baz1078@gmail.com>
2026-03-22 12:39:10 -0500

    fix: add DELETE to CORS allowed methods for admin contractor management

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ CORS(app, resources={r"/api/*": {"origins": "*"}})
 @app.after_request
 def set_headers(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
     return response
 
